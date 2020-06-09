31bf6d6923dcf3480c5e038f5c2a9aa30830c008
Микола Омельянов <mykola.omelyanov@gmail.com>
2020-06-09 18:04:05 +0300

    Initial Commit

---
+++ SidleElement/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SidleElement
+//
+//  Created by Nikolas Omelianov on 09.06.2020.
+//  Copyright © 2020 Nikolas Omelianov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
